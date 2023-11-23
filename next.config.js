6f9fe923fb11dbb031cc1d260ea7296474174b16
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2023-11-23 04:14:50 +0300

    .s

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Output the static files to somewhere more obvious
+    
+  }
+  
+  module.exports = nextConfig
